c91292d68cfe2182c4f4b1aa16697d888eb4f0e4
Kim <kimberlyasharpe@gmail.com>
2016-10-31 17:07:50 -0400

    Added jumping constraints
    When the character is in the air, the player cannot jump repetitively

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,41 @@ public class PlayerController : MonoBehaviour {
 	public float moveSpeed;
 	public float jumpHeight;
 
+	public Transform checkGround;
+	public float checkgroundRadius;
+	// This will be what the ground is assigned to
+	public LayerMask whatisGround;
+	public bool amGrounded;
+	public bool doublejumped;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		amGrounded = Physics2D.OverlapCircle (checkGround.position, checkgroundRadius, whatisGround);
+			
 	}
+
+	void FixedUpdate() 
+	{
+		
+	}
+
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKeyDown (KeyCode.W)) 
+		if (Input.GetKeyDown (KeyCode.W) && amGrounded) 
 		{
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpHeight);
+			amGrounded = false;
+			Debug.Log (amGrounded);
+		} 
+		// If the Player is in the air, player cannot double jump
+		if(GetComponent<Rigidbody2D> ().velocity.y == 0) 
+		{
+			amGrounded = true;
 		}
+
 		if (Input.GetKeyDown (KeyCode.D)) 
 		{
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
@@ -26,5 +49,7 @@ public class PlayerController : MonoBehaviour {
 		{
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (- moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		}
+
+
 	}
 }
