b8c4742a21503f74a97c829902f61903692d5b5f
Kim <kimberlyasharpe@gmail.com>
2016-11-02 18:04:18 -0400

    starting to create camera follow script
    Not done but getting there...

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	private Vector2 velocity;
+	public float smoothTimeY;
+	public float smoothTimeX;
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void LateUpdate()
+	{
+		float posx = Mathf.SmoothDamp(transform.position.x,player.transform.position.x,ref velocity.x, smoothTimeX);
+		float posy = Mathf.SmoothDamp(transform.position.y,player.transform.position.y,ref velocity.y, smoothTimeY);
+
+		transform.position = new Vector3 (posX, posX, transform.position.z);
+	}
+}
